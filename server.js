7c511dab10dee89053c2e4d4bfc10255cfe234cb
Ayush <ayush76a@gmail.com>
2023-07-20 13:13:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// using the api :-> https://type.fit/api/quotes
+// or search for 'quotes generation api free'
+
+// on refershing our page or clicking new quotes button one will automatically get new quotes
+
+//fetch() funciton will call the http of api
+      // the moment response comes it is saved in 'data' variable
+    //   console.log(realData[0].text);
+     
+    //total 1643 quotes on api
